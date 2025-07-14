717e04cc78ee82faa7f0ea9f459b0ffa2c2e1003
AbdullahEnemy <akbuddy8@gmail.com>
2025-07-15 02:19:27 +0500

    api/signup/jwt/integration

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -1,21 +1,36 @@
 const User=require('../models/User');
 const {body,validationResult}=require('express-validator');
+const bcrypt=require('bcryptjs');
+const jwt=require('jsonwebtoken');
+const secretToken=process.env.JWTSecretToken;
 const signupHandler=async(req,res)=>{
-
     const errors=validationResult(req);
     if(!errors.isEmpty()){
         return res.status(400).json({erros:errors.array()});
     }
     const email=req.body.email;
+    const password=req.body.password;
+    const name=req.body.name;
     const user=await User.findOne({email});
     if(user){
-        res.status(400).json("User Already Exists");
+       return  res.status(400).json("User Already Exists");
     }
     try{
-    const data=req.body;
-    const newUser=  await User(data);
-    newUser.save()
-    res.status(201).json({message:"User Successfully Created"});
+    const salt=await bcrypt.genSalt(10);
+    const secPass=await bcrypt.hash(password,salt);
+
+    const newUser=new User();
+    newUser.name=name;
+    newUser.password=secPass;
+    newUser.email=email;
+    await newUser.save()
+    const data={
+        user:{
+            id:newUser.id
+        }
+    }
+    const authToken=jwt.sign(data,secretToken);
+    res.status(201).json({message:"User Successfully Created",Token:authToken});
     }catch(err){
         res.status(500).json({message:"An error has occured"});
     }
