f27a4fcaa17f15d656bfcb51db5c440f404ae780
AbdullahEnemy <akbuddy8@gmail.com>
2025-07-21 01:25:44 +0500

    setup/react-router

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,17 +1,22 @@
-import { useState } from 'react'
 
 import './App.css'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
-import { BrowserRouter, Routes, Route } from "react-router";
+import { BrowserRouter as Router, Routes, Route} from "react-router";
 import { Navbar } from './components/Navbar';
+import { About } from './components/About';
+import { Home } from './components/Home';
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-    <Navbar></Navbar>
-    <h1>this is my react app</h1>
+    <Router>
+      <Navbar>  </Navbar>
+        <Routes>
+          <Route path='/' element={<Home/>}></Route>
+         
+          <Route path='/about' element={<About/>}></Route>
+        </Routes>
+    </Router>
     </>
   )
 }
