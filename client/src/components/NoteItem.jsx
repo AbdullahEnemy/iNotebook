d151a1d66f8fbaf6f6b244cbc7614163937a55dd
AbdullahEnemy <akbuddy8@gmail.com>
2025-07-21 17:32:14 +0500

    component/notes/updatted/edit&delete/button/Added

--- client/src/components/NoteItem.jsx
+++ client/src/components/NoteItem.jsx
@@ -1,17 +1,23 @@
-import React from 'react'
+import React from "react";
 
 export const NoteItem = (props) => {
-    const {note}=props;
+  const { note } = props;
   return (
     <>
-    <div className='col-md-3'>
-    <div className="card my-3" style={{width: "18rem"}}>
-    <div className="card-body">
-    <h5 className="card-title">{note.title}</h5>
-    <p className="card-text">{note.description}</p>
-    </div>
-    </div>
-    </div>
+      <div className="col-md-3">
+        <div className="card my-3" style={{ width: "18rem" }}>
+          <div className="card-body">
+            <div className="d-flex align-items-center">
+              <h5 className="card-title">{note.title}</h5>
+              <div className="ms-auto">
+                <i className="fa-solid fa-trash mx-1"></i>
+                <i className="fa-solid fa-pen-to-square mx-1"></i>
+              </div>
+            </div>
+            <p className="card-text">{note.description}</p>
+          </div>
+        </div>
+      </div>
     </>
-  )
-}
+  );
+};
