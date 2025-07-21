401523a81a6db859595aaf40a23342a7eefc56ef
AbdullahEnemy <akbuddy8@gmail.com>
2025-07-21 18:07:47 +0500

    component/Alert

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -7,12 +7,14 @@ import { Navbar } from './components/Navbar';
 import { About } from './components/About';
 import { Home } from './components/Home';
 import NoteState from './context/notes/NoteState';
+import { Alert } from './components/Alert';
 function App() {
   return (
     <>
     <NoteState>
     <Router>
       <Navbar>  </Navbar>
+      <Alert message="ok"></Alert>
       <div className='container'>
         <Routes>
           <Route path='/' element={<Home/>}></Route>
