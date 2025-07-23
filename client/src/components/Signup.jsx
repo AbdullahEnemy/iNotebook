cd5dee516950c8cedc0c0568a90311c26215323e
AbdullahEnemy <akbuddy8@gmail.com>
2025-07-24 04:28:29 +0500

    component/signup/updatetd

--- client/src/components/Signup.jsx
+++ client/src/components/Signup.jsx
@@ -1,26 +1,66 @@
 import React from 'react'
 
 export const Signup = () => {
+     const [credentials, setCredentials] = useState({name:"", email: "", password: "" });
+    const handleSubmit=async()=>{
+        
+    }
+    const onCHange=()=>{
+        setCredentials({...credentials,[e.target.name]: e.target.value })
+    }
   return (
     <>
     <div className="container">
-        <form>
-  <div className="mb-3">
-    <label htmlFor="exampleInputEmail1" className="form-label">Email address</label>
-    <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"/>
-    
-  </div>
-  <div className="mb-3">
-    <label htmlFor="exampleInputPassword1" className="form-label">Password</label>
-    <input type="password" className="form-control" id="exampleInputPassword1"/>
-  </div>
-  <div className="mb-3 form-check">
-    <input type="checkbox" className="form-check-input" id="exampleCheck1"/>
-    <label className="form-check-label" htmlFor="exampleCheck1">Check me out</label>
-  </div>
-  <button type="submit" className="btn btn-primary">Submit</button>
-</form>
-    </div>
+      <form onSubmit={handleSubmit}>
+                <div className="mb-3">
+          <label htmlFor="name" className="form-label">
+            Name
+          </label>
+          <input
+            type="name"
+            className="form-control"
+            id="name"
+            name="name"
+            
+            onChange={onChange}
+            value={credentials.email}
+          />
+
+        </div>
+        <div className="mb-3">
+          <label htmlFor="email" className="form-label">
+            Email address
+          </label>
+          <input
+            type="email"
+            className="form-control"
+            id="email"
+            name="email"
+            aria-describedby="emailHelp"
+            onChange={onChange}
+            value={credentials.email}
+          />
+
+        </div>
+        <div className="mb-3">
+          <label htmlFor="password" className="form-label">
+            Password
+          </label>
+          <input
+            type="password"
+            value={credentials.password}
+            className="form-control"
+            id="password"
+            name="password"
+            onChange={onChange}
+          />
+        </div>
+        <button type="submit" className="btn btn-primary">
+          SignUp
+        </button>
+      </form>
+      </div>
     </>
+    
   )
 }
