fd40062ead4fffc68515f9e226bb81d49b26bc74
AbdullahEnemy <akbuddy8@gmail.com>
2025-07-24 03:15:24 +0500

    component/signup

--- client/src/components/Login.jsx
+++ client/src/components/Login.jsx
@@ -46,9 +46,7 @@ export const Login = () => {
             onChange={onChange}
             value={credentials.email}
           />
-          <div id="emailHelp" className="form-text">
-            We'll never share your email with anyone else.
-          </div>
+
         </div>
         <div className="mb-3">
           <label htmlFor="password" className="form-label">
