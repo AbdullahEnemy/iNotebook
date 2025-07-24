2ade719b015c7d7fa7127d301f80e74674f1d08b
AbdullahEnemy <akbuddy8@gmail.com>
2025-07-25 03:03:41 +0500

    controllers/notes/updated

--- server/controllers/notes.js
+++ server/controllers/notes.js
@@ -4,23 +4,28 @@ const User=require('../models/User');
 const {body,validationResult}=require('express-validator');
 const getAllNotes = async (req, res) => {
   try {
+    let success=false;
     const userId = req.user.id;
     const allNotes = await Notes.find({user:userId });
+    success=true;
     return res.status(200).json({
       message: "Notes fetched successfully",
       notes: allNotes,
+      success
     });
   } catch (err) {
     console.error(err);
-   return  res.status(500).json({ message: "Server error while fetching notes" });
+    success=false;
+   return  res.status(500).json({ message: "Server error while fetching notes",success });
   }
 };
 
 const addNotes=async(req,res)=>{
+  let success=false;
     const userId=req.user.id;
     const errors=validationResult(req);
         if(!errors.isEmpty()){
-            return res.status(400).json({erros:errors.array()});
+            return res.status(400).json({erros:errors.array(),success});
         }
     const{title,description,tag}=req.body;
     try{
@@ -30,24 +35,27 @@ const addNotes=async(req,res)=>{
         tag,
         user: req.user.id  
     });
+    success=true;
     const savedNotes=await newNote.save();
-    return res.status(201).json({message:"Note saved Successfully",savedNotes});
+    return res.status(201).json({message:"Note saved Successfully",savedNotes,success});
     }
     catch(err){
         console.log(err);
-       return  res.status(500).json({message:"Falied to save note"});
+        success=false;
+       return  res.status(500).json({message:"Falied to save note",success});
     }
 
 }
 const updateNotes=async(req,res)=>{
+  let success=false;
    const userId=req.user.id;
    const notesId=req.params.id;
    let note=await Notes.findById(notesId);
    if(!note){
-    return res.status(404).json({message:"Not Found"})
+    return res.status(404).json({message:"Not Found",success})
    }
    if(userId!==note.user.toString()){
-    return res.status(403).json({message:"Unauthorized action"})
+    return res.status(403).json({message:"Unauthorized action",success})
    }
    const{title,description,tag}=req.body;
    try
@@ -57,31 +65,35 @@ const updateNotes=async(req,res)=>{
     if(description){newNotes.description=description};
     if(tag){newNotes.tag=tag};
     note=await Notes.findByIdAndUpdate(notesId,{$set:newNotes},{new:true});
-    return res.status(200).json({message:"Note Updated successfully"});
+    success=true;
+    return res.status(200).json({message:"Note Updated successfully",success});
    }
    catch(err){
+    success=false;
         console.log(err);
-        return  res.status(500).json({message:"Falied to update note"});
+        return  res.status(500).json({message:"Falied to update note",success});
 
    }
 }
 const deleteNote=async(req,res)=>{
+  let success=false;
    const userId=req.user.id;
    const notesId=req.params.id;
    let note=await Notes.findById(notesId);
    if(!note){
-    return res.status(404).json({message:"Not Found"})
+    return res.status(404).json({message:"Not Found",success})
    }
    if(userId!==note.user.toString()){
-    return res.status(403).json({message:"Unauthorized action"})
+    return res.status(403).json({message:"Unauthorized action",success})
    }
    try{
+    success=true;
         const deletenote=await Notes.findByIdAndDelete(notesId);
-        return res.status(200).json({message:"Note deleted successfully"},deletenote);
+        return res.status(200).json({message:"Note deleted successfully",success});
    }
    catch(err){
         console.log(err);
-        return  res.status(500).json({message:"Falied to delete note"});
+        return  res.status(500).json({message:"Falied to delete note"},success);
    }
 
 }
