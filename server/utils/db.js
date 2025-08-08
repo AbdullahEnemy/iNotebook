cfd9a33cbb38749a0ac27020a65e94f8bf979535
AbdullahEnemy <akbuddy8@gmail.com>
2025-08-08 14:47:07 +0500

    utils/db.js/console.log/removed

--- server/utils/db.js
+++ server/utils/db.js
@@ -1,7 +1,6 @@
 require('dotenv').config();
 const mongoose=require("mongoose");
 const mongoString = process.env.MongoURL;
-console.log("mongoString",mongoString);
 const connectToMongo = async () => {
     try {
         await mongoose.connect(mongoString, {});
